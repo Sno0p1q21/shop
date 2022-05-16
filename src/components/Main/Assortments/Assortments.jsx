06d3266c094c811fb8f7685c7fb2ddc7c34edf97
Ivan <zombie7321@gmail.com>
2022-05-16 21:21:00 +0400

    Правка всплывающего окна для категорий

--- src/components/Main/Assortments/Assortments.jsx
+++ src/components/Main/Assortments/Assortments.jsx
@@ -21,37 +21,63 @@ const Assortments = () => (
     <div className={cx('assortments-list')}>
       <div className={cx('assortments-list__top')}>
         <section className={cx('assortments-list__item')}>
-          <img src={sewage} alt="brick" width="335px" height="235px" />
-          <div className={cx('assortments-list__button-card')}>Трубы канализационные</div>
+          <div className="info-card">
+            <img src={sewage} alt="brick" width="335px" height="235px" />
+            <div className={cx('assortments-list__button-card')}>Трубы канализационные</div>
+            <div className="hidden-card">
+              <a href="/docs/sewerage/Канализация и фитинги.docx" download="Канализация и фитинги.docx">Канализация и фитинги</a>
+            </div>
+          </div>
         </section>
         <section className={cx('assortments-list__item')}>
-          <img src={ppr} alt="brick" width="335px" height="235px" />
-          <div className={cx('assortments-list__button-card')}>ППР трубы и фитинги</div>
+          <div className="info-card">
+            <img src={ppr} alt="brick" width="335px" height="235px" />
+            <div className={cx('assortments-list__button-card')}>ППР трубы и фитинги</div>
+            <div className="hidden-card">
+              <a href="/docs/polypropylene/Отопление.docx" download="Отопление.docx">Отопление</a>
+            </div>
+          </div>
         </section>
         <section className={cx('assortments-list__item')}>
-          <img src={preview6} alt="brick" width="335px" height="235px" />
-          <div className={cx('assortments-list__button-card')}>Трубы водоснабжения</div>
+          <div className="info-card">
+            <img src={preview6} alt="brick" width="335px" height="235px" />
+            <div className={cx('assortments-list__button-card')}>Трубы водоснабжения</div>
+            <div className="hidden-card">
+              <a href="/docs/water_supply/Трубы водоснабжения.docx" download="Трубы водоснабжения.docx">Трубы водоснабжения</a>
+              <a href="/docs/water_supply/Фитинги литые.docx" download="Фитинги литые.docx">Фитинги литые</a>
+              <a href="/docs/water_supply/Фитинги сварные.docx" download="Фитинги сварные.docx">Фитинги сварные</a>
+              <a href="/docs/water_supply/Фитинги электросварные.docx" download="Фитинги электросварные.docx">Фитинги электросварные</a>
+            </div>
+          </div>
         </section>
         <section className={cx('assortments-list__item')}>
-          <img src={preview3} alt="brick" width="335px" height="235px" />
-          <div className={cx('assortments-list__button-card')}>Стальные трубы и фитинги</div>
+          <div className="info-card">
+            <img src={preview3} alt="brick" width="335px" height="235px" />
+            <div className={cx('assortments-list__button-card')}>Стальные трубы и фитинги</div>
+            <div className="hidden-card">
+              <a href="/docs/steel/Газоснабжение.docx" download="Газоснабжение.docx">Газоснабжение</a>
+            </div>
+          </div>
         </section>
         <section className={cx('assortments-list__item')}>
-          <img src={details} alt="brick" width="335px" height="235px" />
-          <div className={cx('assortments-list__button-card')}>Детали трубопроводов</div>
-          {/* <div className={cx('info-card')}>
-            <div>Фитинги </div>
-            <div>Фитинги </div>
-            <div>Фитинги </div>
-          </div> */}
+          <div className="info-card">
+            <img src={details} alt="brick" width="335px" height="235px" />
+            <div className={cx('assortments-list__button-card')}>Детали трубопроводов</div>
+          </div>
         </section>
         <section className={cx('assortments-list__item')}>
-          <img src={another} alt="brick" width="335px" height="235px" />
-          <div className={cx('assortments-list__button-card')}>И многое другое</div>
+          <div className="info-card">
+            <img src={another} alt="brick" width="335px" height="235px" />
+            <div className={cx('assortments-list__button-card')}>И многое другое</div>
+            <div className="hidden-card">
+              <a href="/docs/other/Крепежи.docx" download="Крепежи.docx">Крепежи</a>
+            </div>
+          </div>
         </section>
       </div>
+
       <button className={cx('assortments-list__button')} type="button">
-        <a href="/list.docx" download="list.docx">Полный перечень</a>
+        <a href="/docs/list.docx" download="list.docx">Полный перечень</a>
       </button>
     </div>
   </Container>
